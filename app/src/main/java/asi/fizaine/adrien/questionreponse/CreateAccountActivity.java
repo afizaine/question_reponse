76cb0d65479cfcec0130625984d9da59e4bb4ecb
Adrien Fizaine <adrien.fizaine@outlook.fr>
2018-02-09 15:56:02 +0100

    init

---
+++ app/src/main/java/asi/fizaine/adrien/questionreponse/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package asi.fizaine.adrien.questionreponse;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Adrien on 09/02/2018.
+ */
+
+public class CreateAccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- app/src/main/java/asi/fizaine/adrien/questionreponse/LoginActivity.java
+++ app/src/main/java/asi/fizaine/adrien/questionreponse/LoginActivity.java
@@ -1,7 +1,11 @@
 package asi.fizaine.adrien.questionreponse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /* Récup des champs de saisie */
+        final EditText inputPseudo = (EditText) findViewById(R.id.inputPseudo);     // Pseudo
+        final EditText inputMdp = (EditText) findViewById(R.id.inputPassword);      // Mot de passe
+
+        /* Récup des boutons */
+        final Button btnConnecter = (Button) findViewById(R.id.btnConnexion);       // Se connecter
+        final Button btnCreerCompte = (Button) findViewById(R.id.btnCreerCompte);   // Créer un compte
+
+        /* Clic sur le bouton "Créer un compte" */
+        btnCreerCompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
